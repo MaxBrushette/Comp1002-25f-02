334afae4011aab809b993cd780a8ec32f5aada4c
MaxBrushette <emilycat167@gmail.com>
2025-11-03 01:25:51 -0500

    actually finished forms I think. Also begun PHP form.

---
+++ assignment-1/pizza-process-form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Pizza Processed</title>
+    </head>
+    <body>
+        <header>Pizza-Processing</header>
+        <main>
+            <?php
+
+            $fname = $_POST["fname"];
+
+            ?>
+        </main>
+    </body>
