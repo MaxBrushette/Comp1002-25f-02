f5e76841228761db6b2a9172d47f49e1863d8ac5
MaxBrushette <emilycat167@gmail.com>
2025-11-03 02:20:54 -0500

    finished php form, I hope.

--- assignment-1/pizza-process-form.php
+++ assignment-1/pizza-process-form.php
@@ -11,6 +11,61 @@
 
             $fname = $_POST["fname"];
 
+            $lname = $_POST["lname"];
+
+            $amount = $_POST['amount-pizza'];
+
+            $selectedSize = $_POST['size'];
+
+            $selectedShape = $_POST['shape'];
+
+            $selectedSauce = $_POST['sauce'];
+
+            if (isset($_POST['toppings'])){
+                $selectedToppings = $_POST['toppings'];
+
+                echo('<p>Your selected toppings are: <ul>');
+                foreach($selectedToppings as $toppings){
+                    echo('<li>'.$toppings.'</li>');
+                }
+
+                echo('</ul');
+            }else{
+                echo('<p>No toppings set.</p>');
+            }
+
+            $selectedCrust = $_POST['crust-type'];
+
+            $selectedRetrievalMethod = $_POST['retrieve'];
+
+            if ($selectedRetrievalMethod == "delivery-pizza" ){
+                echo('<p>You have selected delivery.</p>')
+            }elseif($selectedRetrievalMethod == "carry-out-pizza"){
+                echo('<p>You have selected carry-out.')
+            }
+
+            $phone = $_POST['phone'];
+
+            if(isset($_POST['city-pizza'])){
+                $city = $_POST['city-pizza'];
+            }
+            if(isset($_POST['address-line-one-pizza'])){
+                $addresslineone = $_POST['address-line-one-pizza'];
+            }
+            if(isset($_POST['address-line-two-pizza'])){
+                $addresslinetwo = $_POST['address-line-two-pizza'];
+            }
+            if(isset($_POST['province'])){
+                $selectedProvince = $_POST['province'];
+            }
+            if(isset($_POST['postal-code-pizza'])){
+                $zipcode = $_POST['postal-code-pizza'];
+            }
+            if(isset($_POST['retrieve-info'])){
+                $retrieveInfo = $_POST['retrieve-info'];
+            }
+            echo('<p>Thank you for your order,' .$fname.' '.$lname.'!</p>');
             ?>
         </main>
     </body>
+</html>
