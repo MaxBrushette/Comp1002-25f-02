fccf625904e40d093992be2645960d14c79acca5
MaxBrushette <emilycat167@gmail.com>
2025-12-12 02:32:01 -0500

    Added more comments. Finalized.

--- project-1/js/javascript.js
+++ project-1/js/javascript.js
@@ -1,12 +1,16 @@
+
+//Makes sure the document is loaded.
 document.addEventListener("DOMContentLoaded", function () {
     const supportform = document.getElementById("support");
     const reviewform = document.getElementById("review");
-
+//If the support form is uploaded, there's an alert telling the user it was sent.
+//The ifs are so the alerts can be different from each other.
     if(supportform){
         supportform.addEventListener("submit",function(){
             alert("Support form sent! You should get a response within 24-48 hours!");
         })
     }
+//If the review form is uploaded, there's an alert telling the user it was sent.
     if(reviewform){
         reviewform.addEventListener("submit",function(){
             alert("Review sent to our team, thank you!");
