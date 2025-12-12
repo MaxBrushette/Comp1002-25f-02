9fe11aaac5fd5303e6e9fc4733a2462ee8528e91
MaxBrushette <emilycat167@gmail.com>
2025-12-12 00:46:33 -0500

    Finish Support html and Support php and Make Edits

---
+++ support-process.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Support-Request-Processed</title>
+    </head>
+    <body>
+        <header>Support</header>
+        <main>
+            <?php
+                $fname = $_POST["fname"];
+                $lname = $_POST["lname"];
+                $emailaddress = $_POST["emailaddress"];
+                $supporttext = $_POST["support-text"];
+                ?>
+        </main>
+    </body>
+</html>
